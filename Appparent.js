7ad98c88daa6bda06420766e552b8e75f1b5aebb
Chulindra Rai <87846923+IotchulindraRai@users.noreply.github.com>
2023-08-12 14:24:15 +0530

    Create Appparent.js
    App.js Parent

---
+++ Appparent.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Child from './Child';
+function App() {
+  const data="i am chulindraa";
+  return (
+    
+<>
+<Child data={data}/>
+</>
+
+    );
+
+  }
+  export default App;
